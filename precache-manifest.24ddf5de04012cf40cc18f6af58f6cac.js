9e89fde6d3df533998577d1fcfea69a323011209
ManuDeveloper <manuepeva@gmail.com>
2020-08-07 07:45:12 -0500

    Updates

---
+++ precache-manifest.24ddf5de04012cf40cc18f6af58f6cac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7f1c34d8186378ba4b7b7fa60826d",
+    "url": "/manuepeva/TestGitHub.git/index.html"
+  },
+  {
+    "revision": "8dd45df515068d6f3797",
+    "url": "/manuepeva/TestGitHub.git/static/css/main.a6f3522f.chunk.css"
+  },
+  {
+    "revision": "688c29babb962d8ab17f",
+    "url": "/manuepeva/TestGitHub.git/static/js/2.37318f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manuepeva/TestGitHub.git/static/js/2.37318f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd45df515068d6f3797",
+    "url": "/manuepeva/TestGitHub.git/static/js/main.3d71695f.chunk.js"
+  },
+  {
+    "revision": "1b5feb9e360d76e37437",
+    "url": "/manuepeva/TestGitHub.git/static/js/runtime-main.ab808d35.js"
+  },
+  {
+    "revision": "85ffe6cac5af32e155496dfa76648d19",
+    "url": "/manuepeva/TestGitHub.git/static/media/laura.85ffe6ca.jpg"
+  },
+  {
+    "revision": "8d92105a741cc385dc378550ab8f2900",
+    "url": "/manuepeva/TestGitHub.git/static/media/lucia.8d92105a.jpg"
+  },
+  {
+    "revision": "7ea40eee4d07e373073540afcfa2a285",
+    "url": "/manuepeva/TestGitHub.git/static/media/mariajose.7ea40eee.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf8f1005cc686675aa42c93ba19df7dc.js"
+  "/manuepeva/TestGitHub.git/precache-manifest.24ddf5de04012cf40cc18f6af58f6cac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manuepeva/TestGitHub.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

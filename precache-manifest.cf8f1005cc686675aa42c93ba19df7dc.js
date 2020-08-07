b060d7532fdf8bc33ea2a2dd60d2c5f18637461e
ManuDeveloper <manuepeva@gmail.com>
2020-08-07 07:40:32 -0500

    Updates

---
+++ precache-manifest.cf8f1005cc686675aa42c93ba19df7dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad0106a0b50e0ca0aeebe6d86e35aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd45df515068d6f3797",
+    "url": "/static/css/main.a6f3522f.chunk.css"
+  },
+  {
+    "revision": "688c29babb962d8ab17f",
+    "url": "/static/js/2.37318f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37318f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd45df515068d6f3797",
+    "url": "/static/js/main.3d71695f.chunk.js"
+  },
+  {
+    "revision": "efb29250a47633364567",
+    "url": "/static/js/runtime-main.7cf9b1cb.js"
+  },
+  {
+    "revision": "85ffe6cac5af32e155496dfa76648d19",
+    "url": "/static/media/laura.85ffe6ca.jpg"
+  },
+  {
+    "revision": "8d92105a741cc385dc378550ab8f2900",
+    "url": "/static/media/lucia.8d92105a.jpg"
+  },
+  {
+    "revision": "7ea40eee4d07e373073540afcfa2a285",
+    "url": "/static/media/mariajose.7ea40eee.jpg"
+  }
+]);
